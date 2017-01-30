732e8f7cb5ae2d7cae751aff3f80473d41762317
Prawee Wongsa <prawee@hotmail.com>
2017-01-30 17:52:41 +0700

    add DragAndDropListAsset bundle class

---
+++ assets/DragAndDropListAsset.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://www.konkeanweb.com
+ * 1/30/2017 AD 5:50 PM
+ * @copyright Copyright (c) 2017 served
+ * @author Prawee Wongsa <konkeanweb@gmail.com>
+ * @license BSD-3-Clause
+ */
+namespace prawee\vue\widgets\assets;
+
+use yii\web\AssetBundle;
+
+class DragAndDropAsset extends AssetBundle
+{
+    public $sourcePath = '@npm/vue-drag-and-drop-list/dist';
+
+    public $js = [
+        'vue-drag-and-drop-list.js',
+    ];
+    public $depends = [
+        'prawee\vuejs\VueJsAsset',
+    ];
+}
